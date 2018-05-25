dc6c284bbf11392ed517f0d2bb6f237d3663cfe2
Hamifthi <hamedonil@yahoo.com>
2018-05-25 21:28:08 +0430

    add function for reading users and books from text files

--- library.py
+++ library.py
@@ -1,7 +1,7 @@
 import numpy as np
 class Library:
+    # initializing different lists and dictionaries
     def __init__(self, filename = None):
-        import numpy as np
         self.users = []
         self.books = []
         self.users_dict = {}
@@ -12,3 +12,18 @@ class Library:
             self.users_borrowing_history = {}
         self.get_users()
         self.get_books()
+
+    # function to read users from a text file
+    def get_users(self):
+        with open ('users.txt', 'r') as users_file:
+            for user in users_file:
+                self.users.append(user.rstrip())
+                self.users_dict[user.rstrip()] = []
+                self.users_borrowing_history[user.rstrip()] = []
+
+    # function to read books from a text file
+    def get_books(self):
+        with open ('books.txt', 'r') as books_file:
+            for book in books_file:
+                self.books.append(book.rstrip())
+                self.books_dict[book.rstrip()] = ''    
