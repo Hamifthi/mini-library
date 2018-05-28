707391b398fda9e635663c34e3a54456eb3a73aa
Hamifthi <hamedonil@yahoo.com>
2018-05-28 15:03:27 +0430

    get files name in init function also in get functions

--- library.py
+++ library.py
@@ -1,29 +1,29 @@
 import numpy as np
 class Library:
-    # initializing different lists and dictionaries
-    def __init__(self, filename = None):
+    def __init__(self, users_name_file, books_name_file, borrowing_history_filename = None):
+        import numpy as np
         self.users = []
         self.books = []
         self.users_dict = {}
         self.books_dict = {}
         try:
-            self.load_borrowing_history(filename)
+            self.load_borrowing_history(borrowing_history_filename)
         except FileNotFoundError as e:
             self.users_borrowing_history = {}
-        self.get_users()
-        self.get_books()
-
+        self.get_users(users_name_file)
+        self.get_books(books_name_file)
+            
     # function to read users from a text file
-    def get_users(self):
-        with open ('users.txt', 'r') as users_file:
+    def get_users(self, filename):
+        with open (filename, 'r') as users_file:
             for user in users_file:
                 self.users.append(user.rstrip())
                 self.users_dict[user.rstrip()] = []
                 self.users_borrowing_history[user.rstrip()] = []
 
     # function to read books from a text file
-    def get_books(self):
-        with open ('books.txt', 'r') as books_file:
+    def get_books(self, filename):
+        with open (filename, 'r') as books_file:
             for book in books_file:
                 self.books.append(book.rstrip())
                 self.books_dict[book.rstrip()] = ''    
